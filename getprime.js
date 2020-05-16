8e8a5b80b646c7c8824451783f84fd698496b69a
Precious Adedibu <40187926+Preshy-Jones@users.noreply.github.com>
2020-05-16 21:00:05 +0100

    get prime numbers module

---
+++ getprime.js
@@ -0,0 +1,31 @@
+
+function isPrime(number) {
+    if (number < 2) {
+        return false;
+    }
+
+    for (var i = 2; i < number; i += 1) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function firstNPrimes(n) {
+    var primes = [];
+    var num = 2;
+
+    while (primes.length < n) {
+        if (isPrime(num)) {
+            primes.push(num);
+        }
+
+        num += 1;
+    }
+
+    return primes;
+}
+
+module.exports = firstNPrimes;
