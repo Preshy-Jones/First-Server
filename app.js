de88a8d2c447bb246f07c180677d85986b3fd4e3
Precious Adedibu <40187926+Preshy-Jones@users.noreply.github.com>
2020-05-16 15:48:56 +0100

    updated server

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const port = process.env.PORT || 8001
 app.use(bodyParser.urlencoded({ extended: false }))
 //allowing body parser to take in json data
 app.use(bodyParser.json());
@@ -39,11 +40,6 @@ function firstNPrimes(n) {
 }
 
 
-
-//creating POST request
-app.post('/createdata', function (req, res) {
-});
-
 //creating GET request
 app.get('/primes', function (req, res) {
     //outputting palindrome words 
@@ -51,6 +47,6 @@ app.get('/primes', function (req, res) {
 });
 
 
-app.listen(3000, function () {
-    console.log("started server on port 3000");
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
